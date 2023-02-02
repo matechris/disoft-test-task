4349db821c62170fa03a129073b05c29f0fdcd59
Khrystyna Skrypko <christina.skrypko@gmail.com>
2023-02-02 22:46:14 +0200

    Create task & user models

--- task/models.py
+++ task/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+from user.models import User
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    info = models.CharField(max_length=255)
+    status = models.CharField(max_length=255)
+    # author = models.ManyToManyField(
+    #     to=User, related_name="tasks"
+    # )
+    # doer = models.ManyToManyield(
+    #     to=User, related_name="tasks"
+    # )
+    created_at = models.DateField(auto_now_add=True)
+    updated_at = models.DateField(auto_now=True)
+    image = models.ImageField(upload_to="images", blank=True)
+
+    def __str__(self):
+        return str(self.title)
