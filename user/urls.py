ddb77130f23031f87a28fb9b21fecd1466011164
Khrystyna Skrypko <christina.skrypko@gmail.com>
2023-02-06 20:53:31 +0200

    Add JWTAuthentication

--- user/urls.py
+++ user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from user.views import CreateUserView
 
-
 urlpatterns = [
-    path("register/", CreateUserView.as_view(), name="register")
+    path("register/", CreateUserView.as_view(), name="register"),
+    path("token/", TokenObtainPairView.as_view(), name="token-obtain-pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token-refresh"),
 ]
 
 
