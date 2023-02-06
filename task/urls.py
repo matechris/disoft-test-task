867470d3122b0e9cfdba051d9cd7883f29203350
Khrystyna Skrypko <christina.skrypko@gmail.com>
2023-02-06 22:05:44 +0200

    Update Task views and urls

--- task/urls.py
+++ task/urls.py
@@ -1,11 +1,19 @@
-from django.urls import include, path
+from django.urls import path
 
-from task.views import TaskList, TaskDetail
+from task.views import TaskViewSet
 
+task_list = TaskViewSet.as_view(
+    actions={"get": "list", "post": "create"}
+)
+
+task_detail = TaskViewSet.as_view(
+    actions={"get": "retrieve", "put": "update",
+             "patch": "partial_update", "delete": "destroy"}
+)
 
 urlpatterns = [
-    path("", TaskList.as_view(), name="task-list"),
-    path("<int:pk>/", TaskDetail.as_view(), name="task-detial")
+    path("", task_list, name="task-list"),
+    path("<int:pk>/", task_detail, name="task-detail")
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,31 +1,9 @@
-from rest_framework import mixins, generics
+from rest_framework import viewsets
 
 from .models import Task
 from .serializers import TaskSerializer
 
 
-class TaskList(generics.ListCreateAPIView):
+class TaskViewSet(viewsets.ModelViewSet):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
-
-
-class TaskDetail(
-    mixins.RetrieveModelMixin,
-    mixins.UpdateModelMixin,
-    mixins.DestroyModelMixin,
-    generics.GenericAPIView
-):
-    queryset = Task.objects.all()
-    serializer_class = TaskSerializer
-
-    def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
-
-    def put(self, request, *args, **kwargs):
-        return self.update(request, *args, **kwargs)
-
-    def patch(self, request, *args, **kwargs):
-        return self.partial_update(request, *args, **kwargs)
-
-    def delete(self, request, *args, **kwargs):
-        return self.destroy(request, *args, **kwargs)
